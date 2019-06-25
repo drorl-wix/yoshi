49d4ff434a8cc81c7eb671de93e5e09325b81c10
Ran Yitzhaki <ranyitz@gmail.com>
2019-06-25 19:18:17 +0300

    arrange tests in jest configs

--- test/projects/kitchensink-javascript/jest.production.config.js
+++ test/projects/kitchensink-javascript/jest.production.config.js
@@ -1,15 +1,17 @@
 module.exports = {
   preset: 'jest-puppeteer',
   testMatch: [
-    '**/web-worker.test.js',
     '**/entries.test.js',
     '**/env-vars.test.js',
     '**/externals.test.js',
     '**/loaders.test.js',
     '**/moment.test.js',
-    '**/optimize.test.js',
     '**/statics.test.js',
     '**/transpile-externals.test.js',
+    '**/web-worker.test.js',
+
+    // specific for build
+    '**/optimize.test.js',
   ],
   testEnvironment: require.resolve('../../config/buildEnvironment'),
   transformIgnorePatterns: ['/node_modules/', '/test/'],
